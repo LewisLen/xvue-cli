4e0c0cb44e7c0c36b703034880be8f7942602366
LewisLen <csshack2016@126.com>
2023-04-02 22:06:57 +0800

    feat: 添加模板

--- bin/cli.js
+++ bin/cli.js
@@ -1,10 +1,9 @@
 #! /usr/bin/env node
-console.log(process.argv);
 const program = require("commander");
 const chalk = require("chalk");
 
 // .version()方法可以设置版本，其默认选项为-V和--version，设置了版本后，命令行会输出当前的版本号。
-// 修改帮助信息的首行提示
+// usage -h时输出的首行提示帮助信息
 program
   .version("1.0.0")
   .usage("使用方式是命令+空格+参数的方式，即xvue <command> <options>");
@@ -13,6 +12,7 @@ program
   .command("create <projectName> [options]")
   .description("使用xvue创建一个项目")
   .option("-f --force", "强制覆盖")
+  .option("-d", "使用默认设置")
   .action((projectName, options) => {});
 
 program
@@ -25,6 +25,29 @@ program
     console.log(cmd);
   });
 
+const templates = {
+  templateA: {
+    url: "",
+    description: "模板A",
+  },
+  templateB: {
+    url: "",
+    description: "模板B",
+  },
+};
+
+program
+  .command("list")
+  .description("查看系统所有模板")
+  .action(() => {
+    // Object.keys(template).forEach((t) => {
+    //   console.log(t);
+    // });
+    for (let item in templates) {
+      console.log(`${item}.description`);
+    }
+  });
+
 // 自定义监听事件，输入的不是所需要命令
 program.on("command:*", ([cmd]) => {
   program.outputHelp();
@@ -33,6 +56,7 @@ program.on("command:*", ([cmd]) => {
   process.exitCode = 1;
 });
 
+// xvue -h 或者 xvue --help会根据program的代码输出命令的帮助信息
 // 当用户输入--help输出提示语
 program.on("--help", () => {
   console.log();
