e31786d293394af1e0dcc996af818afba4cc20b8
LewisLen <csshack2016@126.com>
2023-04-05 18:59:51 +0800

    feat: 调整根据模板创建文件demo

--- bin/cli.js
+++ bin/cli.js
@@ -9,14 +9,58 @@ const path = require("path");
 
 // .version()方法可以设置版本，其默认选项为-V和--version，设置了版本后，命令行会输出当前的版本号。
 // usage -h时输出的首行提示帮助信息
-program.version("1.0.0").usage("使用方式是命令+空格+参数的方式，即xvue <command> <options>");
+program
+  .version("1.0.0")
+  .usage("使用方式是命令+空格+参数的方式，即xvue <command> <options>");
 
 program
   .command("create <projectName> [options]")
   .description("使用xvue创建一个项目")
   .option("-f --force", "强制覆盖")
   .option("-d", "使用默认设置")
-  .action((projectName, options) => {});
+  .action((projectName, options) => {
+    console.log("options---", options);
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "projectName",
+          message: "请输入项目名称",
+          default: projectName,
+        },
+        {
+          type: "input",
+          name: "author",
+          message: "请输入作者信息",
+          default: "Len",
+        },
+      ])
+      .then((answers) => {
+        const dirPath = path.resolve(__dirname);
+        const templateFilePath = path.join(dirPath, "../template/a.html");
+        const fileStr = fs.readFileSync(templateFilePath, "utf-8");
+        // let template = ejs.compile(fileStr)({ data: answers });
+        const template = ejs.render(fileStr, { data: answers });
+        const dirname = `${process.cwd()}/${projectName}`;
+        if (!fs.existsSync(dirname)) {
+          fs.mkdirSync(dirname, { recursive: true });
+        }
+        fs.writeFile(`${dirname}/index.html`, template);
+      });
+    // 下载远程模板
+    // download(
+    //   "http://github.com:LewisLen/vue-multiple-h5#main",
+    //   projectName,
+    //   { clone: true },
+    //   (err) => {
+    //     if (err) {
+    //       console.log("下载模板失败");
+    //     } else {
+    //       console.log("下载模板成功");
+    //     }
+    //   }
+    // );
+  });
 
 program
   .command("info")
@@ -51,55 +95,6 @@ program
     }
   });
 
-// 根据模板名下载对应的模板
-program
-  .command("init <projectName>")
-  .description("下载对应模板，根据模板创建项目")
-  .action((projectName) => {
-    console.log("模板", path.join(path.resolve(__dirname), "../template/template-a.html"));
-    console.log("项目名", projectName);
-    inquirer
-      .prompt([
-        {
-          type: "input",
-          name: "projectName",
-          message: "请输入项目名称",
-          default: projectName,
-        },
-        {
-          type: "input",
-          name: "author",
-          message: "请输入作者信息",
-          default: "Len",
-        },
-      ])
-      .then((answers) => {
-        const fileStr = fs.readFileSync(
-          path.join(path.resolve(__dirname), "../template/template-a.html"),
-          "utf-8"
-        );
-        const str = ejs.render(fileStr, { title: answers.projectName });
-        // fs.writeFile(str, path.join(path.resolve(__dirname), "../demo/demo.html"));
-        fs.writeFile(path.join(path.resolve(__dirname), "../dist/demo.html"), str, (err) => {
-          if (err) throw err;
-          console.log(str);
-          console.log("The file has been saved!");
-        });
-      });
-    // download(
-    //   "http://github.com:LewisLen/vue-multiple-h5#main",
-    //   projectName,
-    //   { clone: true },
-    //   (err) => {
-    //     if (err) {
-    //       console.log("下载模板失败");
-    //     } else {
-    //       console.log("下载模板成功");
-    //     }
-    //   }
-    // );
-  });
-
 // 自定义监听事件，输入的不是所需要命令
 program.on("command:*", ([cmd]) => {
   program.outputHelp();
@@ -112,7 +107,11 @@ program.on("command:*", ([cmd]) => {
 // 当用户输入--help输出提示语
 program.on("--help", () => {
   console.log();
-  console.log(`  Run ${chalk.cyan(`xvue <command> --help`)} for detailed usage of given command.`);
+  console.log(
+    `  Run ${chalk.cyan(
+      `xvue <command> --help`
+    )} for detailed usage of given command.`
+  );
   console.log();
 });
 
